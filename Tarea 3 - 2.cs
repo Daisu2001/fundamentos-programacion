1d5472d994770f52f30660d3b6b6078141241be5
Daisu2001 <68257776+Daisu2001@users.noreply.github.com>
2020-07-17 16:12:27 -0500

    Add files via upload

---
+++ Tarea 3 - 2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese cuanto es su salario");
+            double salario = double.Parse(Console.ReadLine());
+
+            if (salario < 1755606)
+            {
+                Console.WriteLine("Usted aplica para la tarifa A");
+            } else if (salario >= 1755606 && salario < 3511212)
+            {
+                Console.WriteLine("usted aplica para la tarifa B");
+            }else if(salario >= 3511212)
+            {
+                Console.WriteLine("usted aplica para la tarifa C");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ tarea 3 -1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_3_desafío_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese cuanto es su salario");
+            double salario = double.Parse(Console.ReadLine());
+
+            if (salario < 1755606)
+            {
+                Console.WriteLine("Usted aplica para la tarifa A");
+            }
+            else if (salario >= 1755606 && salario < 4389015)
+            {
+                Console.WriteLine("usted aplica para la tarifa B");
+            }
+            else if (salario >= 4389015)
+            {
+                Console.WriteLine("usted aplica para la tarifa C");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
