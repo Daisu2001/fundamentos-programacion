5c1890fd6d05bb66c427e2699c70217c06317000
Daisu2001 <68257776+Daisu2001@users.noreply.github.com>
2020-07-24 09:36:53 -0500

    Add files via upload

---
+++ binario-decimal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido, vamos a convertir un numero binario a decimal.");
+            Console.WriteLine("Ingrese b4: ");
+            int b4 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese b3: ");
+            int b3 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese b2: ");
+            int b2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese b1: ");
+            int b1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese b0: ");
+            int b0 = int.Parse(Console.ReadLine());
+
+            int n = (b0 * 1) + (b1 * 2) + (b2 * 4) + (b3 * 8) + (b4 * 16);
+            Console.WriteLine("El numero ingresado es: " + n);
+        }
+    }
+}
