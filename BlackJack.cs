cfa507f1ce3692cd2b380757dcb4211cf9aa914e
Daisu2001 <68257776+Daisu2001@users.noreply.github.com>
2020-07-30 14:27:28 -0500

    Add files via upload

---
+++ BlackJack.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random aleatorio = new Random();
+            int cartas = 0, total = 0;
+            string continuar = "s";
+
+            cartas = aleatorio.Next(1, 10);
+            Console.WriteLine("carta =" + cartas);
+            total += cartas;
+            Console.WriteLine("total =" + total);
+
+            cartas = aleatorio.Next(1, 10);
+            Console.WriteLine("carta =" + cartas);
+            total += cartas;
+            Console.WriteLine("total =" + total);
+
+            Console.WriteLine("desea continuar?? (s/n)");
+            continuar = Console.ReadLine();
+
+            while (continuar == "s" && total < 21 )
+            {
+                cartas = aleatorio.Next(1, 10);
+
+                Console.WriteLine("carta =" + cartas);
+                total += cartas;
+                Console.WriteLine("total =" + total);
+
+                if (total == 21)
+                {
+                    Console.WriteLine("felicitaciones, ganaste!!");
+                    break;
+                }
+                   
+
+                if (total > 21)
+                {
+                    Console.WriteLine("limite de 21 excedido, no puede continuar");
+                    
+                }else
+                {
+                    Console.WriteLine("desea continuar?? (s/n)");
+                    continuar = Console.ReadLine();
+                }
+               
+            
+
+            }
+            Console.WriteLine("total =" + total);
+            
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Ciclos edad maxmin.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ciclos_1___2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double total = 0;
+            Console.WriteLine("ingrese el numero de datos: ");
+            int n = int.Parse(Console.ReadLine());
+            int i = 0, min = 200, max = 0;
+            string nombremin = "";
+            string nombremax = "";
+
+            while (i < n)
+            {
+                Console.Write("edad:");
+                int edad = int.Parse(Console.ReadLine());
+                Console.Write("nombre: ");
+                string nombre = (Console.ReadLine());
+
+                
+                if (edad > max)
+                {
+                    max = edad;
+                    nombremax = nombre;
+
+                }
+
+
+                if (edad < min)
+                {
+                    min = edad;
+                    nombremin = nombre;
+                }
+
+
+                total += edad;
+                i++;
+            }
+
+            double promedio = total / n;
+            Console.WriteLine("el promedio es:" + promedio);
+            Console.WriteLine("el mayor es: " + nombremax+ "  tiene: " + max);
+            Console.WriteLine("el menor es: " + nombremin + "  tiene: " + min);
+            Console.ReadKey();
+
+            
+           
+
+        }
+    }
+}
