fc9747da09b5dc3218a376e68b561b5ec10a1692
Daisu2001 <68257776+Daisu2001@users.noreply.github.com>
2020-08-13 22:47:54 -0500

    Add files via upload

---
+++ BlackJack_Multiplayer.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace te_destruyo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random aleatorio = new Random();
+            int cartas = 0, total = 0;
+            string continuar = "s";
+            int jugador = 0;
+            int n = 5;
+
+
+            while (jugador < n)
+            {
+
+                Console.WriteLine("bienvenido jugador " + jugador);
+
+                cartas = aleatorio.Next(1, 10);
+                Console.WriteLine("carta =" + cartas);
+                total += cartas;
+                Console.WriteLine("total =" + total);
+
+                cartas = aleatorio.Next(1, 10);
+                Console.WriteLine("carta =" + cartas);
+                total += cartas;
+                Console.WriteLine("total =" + total);
+
+                Console.WriteLine("desea continuar?? (s/n)");
+                continuar = Console.ReadLine();
+
+                while (continuar == "s" && total < 21)
+                {
+                    cartas = aleatorio.Next(1, 10);
+
+                    Console.WriteLine("carta =" + cartas);
+                    total += cartas;
+                    Console.WriteLine("total =" + total);
+
+                    if (total == 21)
+                    {
+                        Console.WriteLine("felicitaciones, ganaste!!");
+                        break;
+                    }
+
+
+                    if (total > 21)
+                    {
+                        Console.WriteLine("limite de 21 excedido, no puede continuar");
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("desea continuar?? (s/n)");
+                        continuar = Console.ReadLine();
+                    }
+
+                    Console.WriteLine("total =" + total);
+
+                }
+
+                total = 0;
+                Console.WriteLine("gracias por participar");
+                jugador += 1;
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Ciclos p2_clase.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ciclos_p2___clase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random aleatorio = new Random();
+
+            int dado = 0, total = 0;
+            string continuar = "s", usarDadoEs = "";
+            int countSeis = 0, CountDadoEs = 2;
+            int vidas = 3;
+
+
+            Console.WriteLine("bienvenido");
+
+            dado = aleatorio.Next(1, 7);
+            Console.WriteLine("sacó " + dado);
+            total += dado;
+
+            Console.WriteLine("su total es: " + total + " desea continuar? (s/n)");
+            continuar = Console.ReadLine();
+            while (continuar == "s")
+            {
+
+                Console.WriteLine("Desea usar un dado de doce caras? tiene: " + CountDadoEs + " disponible(s) (s/n)");
+                usarDadoEs = Console.ReadLine();
+
+                if(usarDadoEs == "s" && CountDadoEs != 0)
+                {
+                    dado = aleatorio.Next(1, 13);
+                    Console.WriteLine("sacó " + dado);
+                    total += dado;
+                }
+                else { 
+
+                dado = aleatorio.Next(1, 7);
+                Console.WriteLine("sacó " + dado);
+                total += dado;
+                }
+
+
+
+                if (dado == 1)
+                {
+                    Console.WriteLine("pierde una vida y -10 a puntuación total");
+                    vidas -= 1;
+                    total -= 10;
+
+                }
+
+                Console.WriteLine("Usted tiene " + vidas + " vidas");
+
+                if (dado == 6)
+                {
+                    countSeis = 1;
+                }
+                else 
+                {
+                    countSeis = 0;
+                }
+
+                if (dado == 6 && countSeis == 2 && vidas < 3)
+                {
+                    Console.WriteLine("Vida extra");
+                    vidas += 1;
+                    countSeis = 0;
+                    
+                }
+
+                if(vidas == 0)
+                {
+                    Console.WriteLine("se ha quedado sin vidas");
+                    break;
+                }
+
+
+
+                if (total >= 100)
+                {
+                    Console.WriteLine("ganaste");
+                    break;
+                }
+
+               
+              
+                Console.WriteLine("su total es: " + total + " desea continuar? (s/n)");
+                continuar = Console.ReadLine();
+
+               
+
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
